d176cce085aac149fe746036d0d0ffed28831a40
Rebecca Nicacio <rebecca_nicacio@yahoo.com>
2020-11-11 14:58:51 -0800

    implemented rest of cards and fixed typos in manila and new delhi cards which had caused errors

--- SampleCardsBot/Card.cs
+++ SampleCardsBot/Card.cs
@@ -44,7 +44,7 @@ namespace SampleCardsBot
                 Text = "New Delhi is the capital of India and an administrative district of NCT Delhi. "+
                 "New Delhi is also the seat of all three branches of the government of India, hosting the Rashtrapati Bhavan, Parliament House, and the Supreme Court of India."+
                 " Delhi itself is often considered as a hub for international and trade and has been labelled as a global city with its rising economy and wealth.",
-                Images = new List<CardImage> { new CardImage("https://static1.squarespace.com/static/53787978e4b02899e92292a5/537886cee4b0e8c9b2ae9907/59401a90db29d6d9789077f6/1497884404028/thumb_IMG_0690_1024.jpg?format=1500w") },
+                Images = new List<CardImage> { new CardImage("https://robe-trotting.com/wp-content/uploads/2020/03/delhi-itinerary-cover.jpg") },
                 Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "https://en.wikipedia.org/wiki/New_Delhi") },
             };
 
@@ -84,10 +84,11 @@ namespace SampleCardsBot
             var heroCard = new HeroCard
             {
                 Title = "London, England",
-                Text = "Tokyo, officially Tokyo Metropolis (東京都, Tōkyō-to), is the capital and most populous prefecture of Japan." +
-                " Located at the head of Tokyo Bay, the prefecture forms part of the Kantō region on the central Pacific coast of Japan's main island of Honshu.",
-                Images = new List<CardImage> { new CardImage("https://commons.wikimedia.org/wiki/File:Tokyo_Tower_and_surrounding_area.jpg#/media/File:Tokyo_Tower_and_surrounding_area.jpg") },
-                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "https://en.wikipedia.org/wiki/Tokyo") },
+                Text = "London is the capital and largest city of England and the United Kingdom."+
+                " The city stands on the River Thames in the south-east of England, at the head of its 50-mile (80 km) estuary leading to the North Sea."+
+                " London has been a major settlement for two millennia.",
+                Images = new List<CardImage> { new CardImage("https://centralhousinggroup.com/wp-content/uploads/2019/04/London_skyline-1024x1024.jpg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "https://en.wikipedia.org/wiki/London") },
             };
 
             return heroCard;
@@ -98,10 +99,11 @@ namespace SampleCardsBot
             var heroCard = new HeroCard
             {
                 Title = "Johannesburg, South Africa",
-                Text = "Tokyo, officially Tokyo Metropolis (東京都, Tōkyō-to), is the capital and most populous prefecture of Japan." +
-                " Located at the head of Tokyo Bay, the prefecture forms part of the Kantō region on the central Pacific coast of Japan's main island of Honshu.",
-                Images = new List<CardImage> { new CardImage("https://commons.wikimedia.org/wiki/File:Tokyo_Tower_and_surrounding_area.jpg#/media/File:Tokyo_Tower_and_surrounding_area.jpg") },
-                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "https://en.wikipedia.org/wiki/Tokyo") },
+                Text = "Johannesburg, informally known as Jozi, Joburg, or \"The City of Gold\", is the largest city in South Africa,"+
+                " classified as a megacity, and is one of the 50 largest urban areas in the world."+
+                " It is the provincial capital and largest city of Gauteng, which is the wealthiest province in South Africa.",
+                Images = new List<CardImage> { new CardImage("https://www.covingtontravel.com/wp-content/uploads/2014/07/Johannesburg_.jpg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "https://en.wikipedia.org/wiki/Johannesburg") },
             };
 
             return heroCard;
@@ -112,10 +114,10 @@ namespace SampleCardsBot
             var heroCard = new HeroCard
             {
                 Title = "Beijing, China",
-                Text = "Tokyo, officially Tokyo Metropolis (東京都, Tōkyō-to), is the capital and most populous prefecture of Japan." +
-                " Located at the head of Tokyo Bay, the prefecture forms part of the Kantō region on the central Pacific coast of Japan's main island of Honshu.",
-                Images = new List<CardImage> { new CardImage("https://commons.wikimedia.org/wiki/File:Tokyo_Tower_and_surrounding_area.jpg#/media/File:Tokyo_Tower_and_surrounding_area.jpg") },
-                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "https://en.wikipedia.org/wiki/Tokyo") },
+                Text = "Beijing, alternatively romanized as Peking,] is the capital of the People's Republic of China. "+
+                "It is the world's most populous capital city, with over 21 million residents within an administrative area of 16,410.5 km2.",
+                Images = new List<CardImage> { new CardImage("https://mgeorge3948.files.wordpress.com/2013/09/wp_20130929_011.jpg?w=900") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "https://en.wikipedia.org/wiki/Beijing") },
             };
 
             return heroCard;
@@ -126,10 +128,10 @@ namespace SampleCardsBot
             var heroCard = new HeroCard
             {
                 Title = "Mexico City, Mexico",
-                Text = "Tokyo, officially Tokyo Metropolis (東京都, Tōkyō-to), is the capital and most populous prefecture of Japan." +
-                " Located at the head of Tokyo Bay, the prefecture forms part of the Kantō region on the central Pacific coast of Japan's main island of Honshu.",
-                Images = new List<CardImage> { new CardImage("https://commons.wikimedia.org/wiki/File:Tokyo_Tower_and_surrounding_area.jpg#/media/File:Tokyo_Tower_and_surrounding_area.jpg") },
-                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "https://en.wikipedia.org/wiki/Tokyo") },
+                Text = "Mexico Ciy is the capital and largest city of Mexico and the most-populous city in North America."+
+                " Mexico City is one of the most important cultural and financial centres in the world.",
+                Images = new List<CardImage> { new CardImage("https://static-b50e.kxcdn.com/wp-content/uploads/2019/03/29-Mexico-City-by-zsuriel-1024x1024.jpeg") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "https://en.wikipedia.org/wiki/Mexico_City") },
             };
 
             return heroCard;
--- SampleCardsBot/Dialogs/MainDialog.cs
+++ SampleCardsBot/Dialogs/MainDialog.cs
@@ -61,13 +61,38 @@ namespace SampleCardsBot.Dialogs
                 case "Tokyo":
                     reply.Attachments.Add(Card.GetTokyoCard().ToAttachment());
                     break;
+                case "New Delhi":
+                    reply.Attachments.Add(Card.GetNewDelhiCard().ToAttachment());
+                    break;
+                case "Manila":
+                    reply.Attachments.Add(Card.GetManilaCard().ToAttachment());
+                    break;
                 case "New York":
                     reply.Attachments.Add(Card.GetNewYorkCard().ToAttachment());
+                    break;
+                case "London":
+                    reply.Attachments.Add(Card.GetLondonCard().ToAttachment());
+                    break;
+                case "Johannesburg":
+                    reply.Attachments.Add(Card.GetJohannesburgCard().ToAttachment());
+                    break;
+                case "Beijing":
+                    reply.Attachments.Add(Card.GetBeijingCard().ToAttachment());
+                    break;
+                case "Mexico City":
+                    reply.Attachments.Add(Card.GetMexicoCityCard().ToAttachment());
                     break;
                 default:
                     reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
                     reply.Attachments.Add(Card.GetSeattleCard().ToAttachment());
                     reply.Attachments.Add(Card.GetTokyoCard().ToAttachment());
+                    reply.Attachments.Add(Card.GetNewDelhiCard().ToAttachment());
+                    reply.Attachments.Add(Card.GetManilaCard().ToAttachment());
+                    reply.Attachments.Add(Card.GetNewYorkCard().ToAttachment());
+                    reply.Attachments.Add(Card.GetLondonCard().ToAttachment());
+                    reply.Attachments.Add(Card.GetJohannesburgCard().ToAttachment());
+                    reply.Attachments.Add(Card.GetBeijingCard().ToAttachment());
+                    reply.Attachments.Add(Card.GetMexicoCityCard().ToAttachment());
                     break;
             }
 
@@ -84,7 +109,13 @@ namespace SampleCardsBot.Dialogs
             {
                 new Choice() { Value = "Seattle", Synonyms = new List<string>() { "seattle", "0" } },
                 new Choice() { Value = "Tokyo", Synonyms = new List<string>() { "tokyo", "1" }},
-                new Choice() { Value = "New York", Synonyms = new List<string>() { "new york", "2" }},
+                new Choice() { Value = "New Delhi", Synonyms = new List<string>() { "new delhi", "2" }},
+                new Choice() { Value = "Manila", Synonyms = new List<string>() { "manila", "3" }},
+                new Choice() { Value = "New York", Synonyms = new List<string>() { "new york", "4" }},
+                new Choice() { Value = "London", Synonyms = new List<string>() { "london", "5" }},
+                new Choice() { Value = "Johannesburg", Synonyms = new List<string>() { "new york", "6" }},
+                new Choice() { Value = "Beijing", Synonyms = new List<string>() { "new york", "7" }},
+                new Choice() { Value = "Mexico City", Synonyms = new List<string>() { "new york", "8" }},
             };
 
             return cardOptions;
